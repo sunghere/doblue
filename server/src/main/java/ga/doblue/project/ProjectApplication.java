37bc43bc6e13dba8b19146c26d8a1dc2b1d29261
Sunghere <cisisn@naver.com>
2017-06-20 12:39:08 +0900

    tomcat build

--- server/src/main/java/ga/doblue/project/ProjectApplication.java
+++ server/src/main/java/ga/doblue/project/ProjectApplication.java
@@ -1,15 +1,20 @@
 package ga.doblue.project;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
-public class ProjectApplication extends WebMvcConfigurerAdapter {
+public class ProjectApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProjectApplication.class);
     }
 }
