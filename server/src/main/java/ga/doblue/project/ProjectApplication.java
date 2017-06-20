73daa35dff85e7a1230329a6b6c781452efdf095
Sunghere <cisisn@naver.com>
2017-06-20 13:40:58 +0900

    boots...

--- server/src/main/java/ga/doblue/project/ProjectApplication.java
+++ server/src/main/java/ga/doblue/project/ProjectApplication.java
@@ -2,13 +2,27 @@ package ga.doblue.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+/*
 @SpringBootApplication
 @EnableScheduling
 public class ProjectApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+    }
+}*/
+
+@SpringBootApplication
+@EnableScheduling
+public class ProjectApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProjectApplication.class);
     }
 }
