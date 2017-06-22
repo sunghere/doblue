e8eed9a5e2293e6199a4892939b58c985e730d72
belebele <belebele@nate.com>
2017-06-22 14:55:47 +0900

    commit

--- server/src/main/java/ga/doblue/project/ProjectApplication.java
+++ server/src/main/java/ga/doblue/project/ProjectApplication.java
@@ -5,15 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
-/*
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @SpringBootApplication
 @EnableScheduling
 public class ProjectApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
-}*/
+}
 
+/*
 @SpringBootApplication
 @EnableScheduling
 public class ProjectApplication extends SpringBootServletInitializer {
@@ -25,4 +27,4 @@ public class ProjectApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ProjectApplication.class);
     }
-}
+}*/
--- server/src/main/java/ga/doblue/project/model/Project.java
+++ server/src/main/java/ga/doblue/project/model/Project.java
@@ -9,120 +9,127 @@ import java.io.Serializable;
 public class Project implements Serializable {
 
     private int seq;
-    private String url="";
-    private String img="";
-    private String title="";
-    private String content="";
-    private String sdate="";
-    private String edate="";
+    private String url = "";
+    private String img = "";
+    private String title = "";
+    private String content = "";
+    private String sdate = "";
+    private String edate = "";
     private int completed;
     private int del = 0;
     private String category;
 
-	public Project(int seq, String url, String img, String title, String content, String sdate, String edate,
-			int completed, int del, String category) {
-		super();
-		this.seq = seq;
-		this.url = url;
-		this.img = img;
-		this.title = title;
-		this.content = content;
-		this.sdate = sdate;
-		this.edate = edate;
-		this.completed = completed;
-		this.del = del;
-		this.category = category;
-	}
-
-	@Override
-	public String toString() {
-		return "Project [seq=" + seq + ", url=" + url + ", img=" + img + ", title=" + title + ", content=" + content
-				+ ", sdate=" + sdate + ", edate=" + edate + ", completed=" + completed + ", del=" + del + ", category="
-				+ category + "]";
-	}
-
-	public int getSeq() {
-		return seq;
-	}
-
-	public void setSeq(int seq) {
-		this.seq = seq;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public String getImg() {
-		return img;
-	}
-
-	public void setImg(String img) {
-		this.img = img;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getSdate() {
-		return sdate;
-	}
-
-	public void setSdate(String sdate) {
-		this.sdate = sdate;
-	}
-
-	public String getEdate() {
-		return edate;
-	}
-
-	public void setEdate(String edate) {
-		this.edate = edate;
-	}
-
-	public int getCompleted() {
-		return completed;
-	}
-
-	public void setCompleted(int completed) {
-		this.completed = completed;
-	}
-
-	public int getDel() {
-		return del;
-	}
-
-	public void setDel(int del) {
-		this.del = del;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public Project() {
+    public Project(int seq, String url, String img, String title, String content, String sdate, String edate, int completed, int del, String category) {
+        this.seq = seq;
+        this.url = url;
+        this.img = img;
+        this.title = title;
+        this.content = content;
+        this.sdate = sdate;
+        this.edate = edate;
+        this.completed = completed;
+        this.del = del;
+        this.category = category;
     }
 
-  
+    @Override
+    public String toString() {
+        return "Project{" +
+                "seq=" + seq +
+                ", url='" + url + '\'' +
+                ", img='" + img + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", sdate='" + sdate + '\'' +
+                ", edate='" + edate + '\'' +
+                ", completed=" + completed +
+                ", del=" + del +
+                ", category='" + category + '\'' +
+                '}';
+    }
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSdate() {
+        return sdate;
+    }
+
+    public void setSdate(String sdate) {
+        this.sdate = sdate;
+    }
+
+    public String getEdate() {
+        return edate;
+    }
+
+    public void setEdate(String edate) {
+        this.edate = edate;
+    }
+
+    public int getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(int completed) {
+        this.completed = completed;
+    }
+
+    public int getDel() {
+        return del;
+    }
+
+    public void setDel(int del) {
+        this.del = del;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Project() {
+    }
+
+
 }
