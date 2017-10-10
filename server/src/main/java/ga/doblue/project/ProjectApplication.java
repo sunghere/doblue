578cf7aa430202b51c3149e8eb1eae7f98e6890a
Sunghere <cisisn@naver.com>
2017-10-10 15:23:55 +0900

    tomcat start cofigure

--- server/src/main/java/ga/doblue/project/ProjectApplication.java
+++ server/src/main/java/ga/doblue/project/ProjectApplication.java
@@ -1,28 +1,27 @@
 package ga.doblue.project;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@SpringBootApplication
+/*@SpringBootApplication
 @EnableScheduling
 public class ProjectApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
-}
+}*/
 
-/*
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
 @EnableScheduling
 public class ProjectApplication extends SpringBootServletInitializer {
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
-    }
+    }*/
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ProjectApplication.class);
     }
-}*/
+}
